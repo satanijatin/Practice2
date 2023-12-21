11bdf6b52a424862e5e34c15e81aedb79a07b284
Jatin Satani <jatinsatani111@gmail.com>
2023-12-21 11:44:06 +0530

    Done

--- prac2pro1.py
+++ prac2pro1.py
@@ -8,5 +8,4 @@ for number in range(startvalue, endvalue + 1):
     if number % 7 == 0 and number % 5 == 0:
         result_numbers.append(number)
 
-print("Numbers divisible by 7 and multiples of 5 between", startvalue, "and", endvalue, "are:")
 print(result_numbers)
