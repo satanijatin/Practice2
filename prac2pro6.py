4b161181a996c0093025cf3f683ccf3d03d02ee4
Jatin Satani <jatinsatani111@gmail.com>
2023-12-21 12:00:50 +0530

    Done

---
+++ prac2pro6.py
@@ -0,0 +1,7 @@
+chars=str(input("Enter Value: "))
+vowels = ["a", "e", "i", "o", "u"]
+
+if chars.lower() in vowels:
+  print("The letter  is a vowel.")
+else:
+  print("The letter is not a vowel.")
