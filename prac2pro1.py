9bb7c9405b6397eaa1676b85e92d6306904ccd7e
Jatin Satani <jatinsatani111@gmail.com>
2023-12-21 11:45:14 +0530

    Done

--- prac2pro1.py
+++ prac2pro1.py
@@ -3,9 +3,9 @@ startvalue = 1500
 endvalue = 2700
 
 
-result_numbers = []
+finalvalue = []
 for number in range(startvalue, endvalue + 1):
     if number % 7 == 0 and number % 5 == 0:
-        result_numbers.append(number)
+        finalvalue.append(number)
 
-print(result_numbers)
+print(finalvalue)
